7c3a0382160a54f50225834ef4c8bf3b00079571
stephym <60659457+stephymelo@users.noreply.github.com>
2020-10-15 23:16:27 -0500

    -.-serio

--- src/model/MainView.java
+++ src/model/MainView.java
@@ -6,6 +6,10 @@ import processing.core.PApplet;
 public class MainView extends PApplet{
 
 	PartidaView partida; 
+	ScoresView scorePantalla;
+	ConexionView conexionPantalla;
+	InstruccionesView instruccionesPantalla;
+	private int pantallas;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PApplet.main("model.MainView");
@@ -17,14 +21,39 @@ public class MainView extends PApplet{
 
 	public void setup() {
 		partida = new PartidaView(this); 
+		scorePantalla = new ScoresView(this); 
+		conexionPantalla = new ConexionView(this); 
+		instruccionesPantalla = new InstruccionesView(this); 
 		partida.load();
+		pantallas=0;
 
 	}
 
 	public void draw() {
 		background(255,255,0); 
-		partida.drawIt();
-
+	
+		
+		switch(pantallas) {
+		case 0:
+			//aqui conexion
+			partida.drawIt();
+			break;
+		case 1:
+			//aqui iria play
+			conexionPantalla.drawIt();
+			break;
+		case 2:
+			instruccionesPantalla.drawIt();
+			break;
+		case 3:
+			//aqui iria partida
+			
+			break;
+		case 4:
+			scorePantalla.drawIt();
+			break;
+			
+		}
 	}
 
 }
